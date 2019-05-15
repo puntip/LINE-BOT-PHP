79ff4da79d38fc54da49ea9c448c095952b99634
puntip <40046116+puntip@users.noreply.github.com>
2019-05-15 22:33:13 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -8,18 +8,20 @@ $content = file_get_contents('php://input');
 
 $events = json_decode($content, true);
 // Validate parsed JSON data
-if (!is_null($events['ESP'])) {
-	
-	send_LINE($events['ESP']);
-		
+if (!is_null($events['ESP'])) 
+{
+	send_LINE($events['ESP']);	
 	echo "OK";
-	}
-if (!is_null($events['events'])) {
+}
+if (!is_null($events['events'])) 
+{
 	echo "line bot";
 	// Loop through each event
-	foreach ($events['events'] as $event) {
+	foreach ($events['events'] as $event) 
+	{
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') 
+		{
 			// Get text sent
 			$text = $event['message']['text'];
 			// Get replyToken
@@ -28,14 +30,12 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 
 			$Topic = "NodeMCU1" ;
-			getMqttfromlineMsg($Topic,$text);
-			   
-			
+			getMqttfromlineMsg($Topic,$text);	   	
 		}
 	}
 }
-$Topic = "NodeMCU1" ;
-$text = "Test";
-getMqttfromlineMsg($Topic,$text);
+//$Topic = "NodeMCU1" ;
+//$text = "Test";
+//getMqttfromlineMsg($Topic,$text);
 echo "OK3";
 ?>
